fb03add8c8f2feb24f0bf34702c4f22a99969175
Layomi <layomilawore@gmail.com>
2025-09-15 13:28:26 +0100

    removing unused variables

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Home from "./pages/Home/Home";
 import { Routes, Route, useNavigate, useLocation } from "react-router";
 import Login from "./pages/Login/Login";
 import Player from "./pages/Player/Player";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { auth } from "./firebase";
 import { ToastContainer } from "react-toastify";
@@ -10,11 +10,9 @@ import { ToastContainer } from "react-toastify";
 function App() {
   const navigate = useNavigate();
   const location = useLocation();
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user: User | null) => {
-      setLoading(false);
       if (user) {
         if (location.pathname === "/login") {
           navigate("/");
